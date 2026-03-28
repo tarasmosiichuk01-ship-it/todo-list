d3ea3ab3d2920394bd2c7a8ea78534ec05cc3332
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-03-28 12:35:09 +0200

    Customize task model and rigister tag model in admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "content", )
+    list_filter = ("created_at", "deadline", "is_completed", )
+    search_fields = ("name", )
+
+
+admin.site.register(Tag)
