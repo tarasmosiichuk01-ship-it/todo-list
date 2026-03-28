0263c20fcadbb18b7267916a8a94f2e67a8578ff
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-03-28 14:04:10 +0200

    Add create,update,delete page for task model with templates

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 
 from tasks.views import (
     IndexView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
 )
 
 app_name = "tasks"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task_create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from tasks.models import Task
@@ -8,3 +9,18 @@ class IndexView(generic.ListView):
     template_name = "tasks/index.html"
     context_object_name = "tasks_list"
 
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:index")
